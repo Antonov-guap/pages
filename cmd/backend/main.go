e6d33be5e17aa23d5862c41bc624d88296ba2e9e
aleksandr.antonov <aleksandr.antonov@quadcode.com>
2023-10-02 00:46:56 +0300

    experiment with pages

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -11,10 +11,16 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Static("", ".")
+
 	app.Use(cors.New())
 	app.Get("/time", func(c *fiber.Ctx) error {
 		return c.SendString(time.Now().Format(time.RFC3339))
 	})
 
-	lo.Must0(app.Listen(":8080"))
+	lo.Must0(app.ListenTLS(
+		"0.0.0.0:8080",
+		"../../../local/htmx/cert/conf/live/htmx.space/fullchain.pem",
+		"../../../local/htmx/cert/conf/live/htmx.space/privkey.pem",
+	))
 }
