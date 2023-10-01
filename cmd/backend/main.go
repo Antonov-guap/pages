ddd4d503469c4718288878e8750e84fed7dc6f31
aleksandr.antonov <aleksandr.antonov@quadcode.com>
2023-10-01 13:14:13 +0300

    experiment with pages

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,8 +15,32 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	priv := lo.Must(rsa.GenerateKey(rand.Reader, 2048))
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Your Organization"},
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
+		KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageServerAuth,
+		},
+		BasicConstraintsValid: true,
+	}
+
+	certDER := lo.Must(x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv))
+	tlsCert := tls.Certificate{
+		Certificate: [][]byte{certDER},
+		PrivateKey:  priv,
+	}
+
 	app.Get("/time", func(c *fiber.Ctx) error {
-		return c.SendString(time.Now().String())
+		return c.SendString(time.Now().Format(time.RFC3339))
 	})
-	lo.Must0(app.Listen("0.0.0.0:8080"))
+
+	lo.Must0(app.ListenTLSWithCertificate("0.0.0.0:8080", tlsCert))
 }
